9cd88027eb870eacd85f36497acce4cd22644559
aesbetancourt <aesbetancourt@outlook.com>
2022-10-16 11:15:54 -0400

    Adding theme configs

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,32 @@ import {
   Heading,
   Button,
   Text,
-  Code
+  Code,
+  useColorMode,
 } from "@chakra-ui/react";
 import reactLogo from "./assets/react.svg";
 import chakraLogo from "./assets/chakra.svg";
 
 function App() {
   const [count, setCount] = React.useState(0);
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Center h="100vh">
-      <VStack gap='5'>
+      <VStack gap="5">
         <HStack gap="18">
-          <Image src="/vite.svg" w='10' alt="Vite Logo" />
-          <Image src={reactLogo} w='10' alt="React Logo" />
-          <Image src={chakraLogo} w='10' alt="Chakra Logo" />
+          <Image src="/vite.svg" w="10" alt="Vite Logo" />
+          <Image src={reactLogo} w="10" alt="React Logo" />
+          <Image src={chakraLogo} w="10" alt="Chakra Logo" />
         </HStack>
         <Heading>Vite + React + Chakra UI</Heading>
         <VStack>
-          <Button onClick={() => setCount(count + 1)}>
-            count is {count}
+          <Button onClick={() => setCount(count + 1)}>count is {count}</Button>
+          <Text>
+            Edit <Code>src/App.tsx</Code> and save to test HMR
+          </Text>
+          <Button onClick={toggleColorMode}>
+            Toggle {colorMode === "light" ? "Dark" : "Light"}
           </Button>
-          <Text>Edit <Code>src/App.tsx</Code> and save to test HMR</Text>
         </VStack>
       </VStack>
     </Center>
---
+++ src/theme/index.ts
@@ -0,0 +1,7 @@
+import { extendTheme } from "@chakra-ui/react";
+
+
+// https://chakra-ui.com/docs/styled-system/customize-theme#scaling-out-your-project
+const overrides = {};
+
+export default extendTheme(overrides);
